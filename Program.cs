b805321a72f780b16b063052ec81ced9611fc765
s224433 <aysenujin@gmail.com>
2025-09-21 20:46:11 +0200

    Færdig version

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.Write("Enter username:");
+string username = Console.ReadLine();
+Console.Write("Enter password:");
+string password = Console.ReadLine();
+Console.Write("Enter user ID:");
+uint userId = uint.Parse(Console.ReadLine());
 
-Console.WriteLine("Hello, World!");
+var userIsAdmin = userId >65536;
+var usernameValid = username.Length >= 3;
+var passwordValid = (password.Contains("$") || password.Contains("|") || password.Contains("@")) &&
+                    (userIsAdmin ? password.Length >= 20 : password.Length >= 16);
+
+Console.WriteLine("User is admin" + userIsAdmin);             
+Console.WriteLine("Username is valid" + usernameValid);
+Console.WriteLine("Password is vaild" + passwordValid);
+
+if (usernameValid && passwordValid)
+{
+    Console.WriteLine("Login successful");
+}
+else 
+{
+    Console.WriteLine("Login failed");
+}    
